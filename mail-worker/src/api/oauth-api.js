0ef6ef200136a348fd48179c86f4457f036b05c2
alvin000009238 <107313913+alvin000009238@users.noreply.github.com>
2025-10-16 14:33:42 +0800

    feat: hint github redirect domain

--- mail-worker/src/api/oauth-api.js
+++ mail-worker/src/api/oauth-api.js
@@ -4,12 +4,13 @@ import result from '../model/result';
 
 app.get('/oauth/:provider/authorize', async (c) => {
         const { provider } = c.req.param();
-        const data = await oauthService.authorize(c, provider);
+        const query = c.req.query();
+        const data = await oauthService.authorize(c, provider, { redirectOrigin: query?.redirect_origin });
         return c.json(result.ok(data));
 });
 
 app.post('/oauth/:provider/callback', async (c) => {
         const { provider } = c.req.param();
-        const token = await oauthService.callback(c, provider, await c.req.json());
-        return c.json(result.ok({ token }));
+        const data = await oauthService.callback(c, provider, await c.req.json());
+        return c.json(result.ok(data));
 });

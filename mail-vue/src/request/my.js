6407b4a7dceabbd1b841175eef12bbddad28e5f4
alvin000009238 <alvin980408@gmail.com>
2026-03-03 20:57:49 +0800

    fix(vue): restore missing oauth methods in my.js after merge

--- mail-vue/src/request/my.js
+++ mail-vue/src/request/my.js
@@ -5,10 +5,29 @@ export function loginUserInfo() {
 }
 
 export function resetPassword(password) {
-    return http.put('/my/resetPassword', {password})
+    return http.put('/my/resetPassword', { password })
 }
 
 export function userDelete() {
     return http.delete('/my/delete')
 }
 
+function currentOrigin() {
+    if (typeof window !== 'undefined' && window.location?.origin) {
+        return window.location.origin;
+    }
+
+    return '';
+}
+
+export function myOAuthAuthorize(provider) {
+    return http.get(`/my/oauth/${provider}/authorize`)
+}
+
+export function myOAuthCallback(provider, payload) {
+    return http.post(`/my/oauth/${provider}/callback`, payload)
+}
+
+export function myOAuthUnbind(provider) {
+    return http.delete(`/my/oauth/${provider}`)
+}

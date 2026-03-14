717ffce9ddad628674d836e6bd7393cbe4040ab0
alvin000009238 <alvin980408@gmail.com>
2026-03-14 16:16:06 +0800

    feat: implement application header component with user profile, theme switching, and navigation controls.

--- mail-vue/src/layout/header/index.vue
+++ mail-vue/src/layout/header/index.vue
@@ -16,7 +16,7 @@
       <div v-else class="dark-icon icon-item" @click="openDark($event)">
         <Icon icon="solar:moon-linear"/>
       </div>
-      <div class="notice icon-item" @click="openNotice">
+      <div v-if="settingStore.settings.notice !== 1" class="notice icon-item" @click="openNotice">
         <Icon icon="streamline-plump:announcement-megaphone"/>
       </div>
       <el-dropdown ref="userinfoRef" @visible-change="e => userInfoShow = e" :teleported="false" popper-class="detail-dropdown">
